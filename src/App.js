911665c7cada311f62b13fa3352c2668c5cd07fc
StevenPurn <smp5119@gmail.com>
2018-08-07 17:32:11 -0700

    Adding routes & removing state

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router';
 import logo from './twitch-logo.png';
 import './App.css';
-import GameAnalytics from './Components/GameAnalytics';
-import ChatAnalytics from './Components/ChatAnalytics';
-import fetchTwitch from './fetch-twitch';
+import AnalyticsPage from './Components/AnalyticsPage';
+import Home from './Components/Home';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      streamDetails: {
-        user: 'billy1kirby',
-        game_id: '394568',
-        viewers: '0',
-      }
-    }
-  }
-
-  componentDidMount() {
-    const username = this.state.streamDetails.user;
-    const url = `https://api.twitch.tv/helix/streams?user_login=${username}`
-    fetchTwitch(url)
-    .then((data) => {
-      if (data['data'].length > 0) {
-        const streamerData = data['data'][0];
-        console.log(streamerData);
-        if (streamerData['type'] === 'live') {
-          console.log('streamer is currently live');
-          this.setState({
-            streamDetails: {
-              game_id: streamerData['game_id'],
-              viewers: streamerData['viewer_count'],
-            }
-          });
-        }
-      } else {
-        console.log('streamer is currently offline');
-      }
-    })
-    .catch((err) => {
-      throw err;
-    });
-  }
-
   render() {
     return (
       <div className="App">
@@ -50,8 +13,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Twitch Analytics</h1>
         </header>
-        <GameAnalytics gameId={this.state.streamDetails.game_id} viewers={this.state.streamDetails.viewers} />
-        <ChatAnalytics channel={this.state.streamDetails.user} />
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/:username' component={AnalyticsPage}/>
+        </Switch>
       </div>
     );
   }
