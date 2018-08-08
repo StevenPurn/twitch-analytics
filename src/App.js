5a8337898b4bc2f475ccb5bbf2f833f7c11dc2a8
StevenPurn <smp5119@gmail.com>
2018-08-07 17:40:19 -0700

    Making App a stateless functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router';
 import logo from './twitch-logo.png';
 import './App.css';
 import AnalyticsPage from './Components/AnalyticsPage';
 import Home from './Components/Home';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Twitch Analytics</h1>
-        </header>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/:username' component={AnalyticsPage}/>
-        </Switch>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Twitch Analytics</h1>
+      </header>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/:username' component={AnalyticsPage}/>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
